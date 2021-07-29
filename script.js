05b38b72bdb2aa6e3ecf6def0ea50bf446412102
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-29 18:10:12 -0400

    more JS, removed API key var

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-var APIKey = "d04c941f9a17d4fd069f2142973171ec";
-
-var city;
+// Declaring Variables: -----------------------------------------------------------------//
+var goBtn =  document.querySelector('.goBtn');
+var city = document.querySelector('.city');
+var searchedCities = [];
 
 function getForecast() {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    fetch(queryURL);
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=d04c941f9a17d4fd069f2142973171ec";
+    fetch(queryUrl)
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then (function (data) {
+
+
+    })
+
 
 };
 
-getForecast();
+function get5DayForecast() {
+    var queryUrl = "api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&appid=d04c941f9a17d4fd069f2142973171ec";
+    fetch(queryUrl)
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then (function (data) {
+
+    })
+}
+
+goBtn.addEventListener('click', getForecast, get5DayForecast);
+
+
