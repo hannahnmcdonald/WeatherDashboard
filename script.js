6075e4a123b4d17729cc8ad29db5ab36f90e1f92
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-30 21:04:15 -0400

    routine commit- changed recent history and card structure + adding js

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
-// Declaring Variables: -----------------------------------------------------------------//
-var goBtn =  document.querySelector('.goBtn');
-var city = document.querySelector('.city');
-var searchedCities = [];
+// // Declaring Variables: -----------------------------------------------------------------//
+// var goBtn =  document.querySelector('.goBtn');
+// var city = document.querySelector('.city');
+// var searchedCities = [];
 
-function getForecast() {
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=d04c941f9a17d4fd069f2142973171ec";
-    fetch(queryUrl)
-    .then(function (response) {
-      return response.json();
-    })
+// function getForecast() {
+//     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=d04c941f9a17d4fd069f2142973171ec";
+//     fetch(queryUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
 
-    .then (function (data) {
-
-
-    })
+//     .then (function (data) {
 
 
-};
+//     })
 
-function get5DayForecast() {
-    var queryUrl = "api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&appid=d04c941f9a17d4fd069f2142973171ec";
-    fetch(queryUrl)
-    .then(function (response) {
-      return response.json();
-    })
 
-    .then (function (data) {
+// };
 
-    })
+// function get5DayForecast() {
+//     var queryUrl = "api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&appid=d04c941f9a17d4fd069f2142973171ec";
+//     fetch(queryUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+
+//     .then (function (data) {
+
+//     })
+// }
+
+// goBtn.addEventListener('click', getForecast, get5DayForecast);
+
+
+// Declaring Variables:-----------------------------------//
+var historyEl = document.querySelector('#history');
+
+
+
+
+function displaySavedLocations() {
+    var locations = ['Chicago','London', 'Munich'];
+    // <li><button data-location="Chicago">Chicago</button></li>
+    locations.forEach(function(item){
+    var listItem = document.createElement('li');
+    var content = `<button data-location="${item}">${item}</button>`
+    listItem.innerHTML = content;
+    historyEl.appendChild(listItem);
+    });
+    
 }
 
-goBtn.addEventListener('click', getForecast, get5DayForecast);
 
+function init() {
+    displaySavedLocations();
+}
 
+init();
