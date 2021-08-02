6f6d70c23b89218a7b143657a9dca4c4f08214f8
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-08-01 20:49:24 -0400

    first API working, still working on second

--- script.js
+++ script.js
@@ -2,11 +2,21 @@
 var historyEl = document.querySelector('#history');
 var goButtonEl = document.querySelector('.goBtn');
 var citySearch = document.querySelector('.city');
+var cardDeck = document.querySelector('.card-deck');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+var humidity = document.querySelector('#humidity');
+var UVIndex = document.querySelector('#UVIndex');
+var UVIndex1 = document.querySelector('#UVIndex1');
+var cardDate = document.querySelector('#date');
+var photo = document.getElementById('img');
 // Declaring Constants:----------------------------------//
 const apiUrl = "https://api.openweathermap.org";
 const apiKey = "d04c941f9a17d4fd069f2142973171ec";
 
+
 // console.log(citySearch);
+
 
 
 // Display the recent searches in a list order----------------//
@@ -60,6 +70,8 @@ function handleSuccessfulLocationFetch(data, location) {
 // Retrieving API data for inputted location-----------------------------------------//
 function getLocation(event) {
     event.preventDefault();
+    // Boxes are formed when function is run- changes css from display none to display flex
+    cardDeck.style.display = "flex";
     var location = citySearch.value;
     if (!location) {
         window.alert('Please enter a location!');
@@ -68,6 +80,7 @@ function getLocation(event) {
 
     // TEST: console.log(location);
     // TEST: console.log(url);
+
     
     fetch(url).then(function (response) {
         if (!response.ok) {
@@ -84,27 +97,98 @@ function getLocation(event) {
         
         }
 
+        temp.textContent = data.main.temp;
+        wind.textContent = data.wind.speed;
+        humidity.textContent = data.main.humidity;
+
+
+
+        // TEST:
+        // console.log(data.main.temp)
+        // console.log(data.coord.lon)
+        // console.log(data.coord.lat)
+        // console.log(data.wind.speed)
+        // console.log(data.main.humidity)
+
         handleSuccessfulLocationFetch(data, location);
 
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        // console.log(lat);
+        // console.log(lon);
+        getFiveDayForecast(lat, lon);
     })
     .catch(function() {
         window.alert("Something went wrong");
     });
 
-    
+
 };
 
+function getFiveDayForecast(lat, lon) {
+    var fiveUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + apiKey;
+    fetch(fiveUrl)
+    .then(function (response) {
+      if (response.ok){ 
+          response.json()
+          .then(function (data){
+              console.log("data", data);
 
+              for (i = 0; i < 5 ; i++) {
+
+                // Set Date
+                var time = moment().add();
+                var date = (time.format('MM/D/YY'));
+
+                // Set weather photo
+                // var photo = data.daily[i].weather[0].icon;
+                // var photoSrc = "http://openweathermap.org/img/wn/" + photo + "@2x.png";
+                // photo.setAttribute('src', photoSrc);
+                
+                // TEST: console.log(time);
+                // TEST: console.log(date);
+
+                // temp.textContent = data.daily[i].temp.day
+                // wind.textContent = data.daily[i].weather.wind_speed;
+                // humidity.textContent = data.daily[i].feels_like.humidity;
+                
+                
+                cardDate.textContent = date;
+                console.log(cardDate);
+                
+            }
+
+            // Passes UV Index data to current data weather card //
+            UVIndex1.textContent = data.daily[i].uvi;
+            UVIndex.textContent = data.daily[i].uvi;
+
+            
+            setUltraviolentIndex(UVIndex);
+          });
+        
+        }
+    })
+};
+
+function setUltraviolentIndex(UVIndex) {
+    if (UVIndex <= 2) {
+        $("#UVIndex").attr("color", "green");
+    };
+    if (UVIndex > 2 && UVIndex <= 5) {
+        $("#UVIndex").attr("color", "yellow");
+    };
+    if (UVIndex > 5) {
+        $("#UVIndex").attr("color", "red");
+    };
+};
 
 //------------------------------------------------------------------//
 
 function updateContentPane(event) {
     var buttonClicked = event.target;
-    // console.log(citySearch.value);
+    // TEST: console.log(citySearch.value);
     var location = buttonClicked.getAttribute("data-location");
-    // window.alert(location);
     console.log(location);
-    // getForecast(location)
 };
 
 function setEventListeners() {
@@ -123,22 +207,4 @@ init();
 
 // -------------------------------------------------------//
 
-// var searchedCities = [];
 
-
-
-
-// function get5DayForecast(city) {
-//     var queryUrl = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=d04c941f9a17d4fd069f2142973171ec&units=imperial";
-//     fetch(queryUrl)
-//     .then(function (response) {
-//       if (response.ok){ 
-//           response.json().then(function (data){
-//               console.log(data);
-//           });
-//         }
-//           else {alert('invalid!')};
-//     })
-// };
-
-// goButtonEl.addEventListener('click', getForecast, get5DayForecast);
